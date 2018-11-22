40fa5e242cce025b4dc08e7ad4ea7567f200a7c0
juan <j.m.bonomo1982@gmail.com>
2018-11-22 02:20:35 -0300

    Template modificado

--- verControl/views.py
+++ verControl/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Determinacion
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    deter = Determinacion.objects.all()
+    return render(request, 'verControl/index.html', {'deter':deter})
